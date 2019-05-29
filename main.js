17f293a797a163f471db88e3d37a95e8ffc4b5d4
= <=>
2019-05-30 01:17:07 +0200

    added camera listing support

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const edge = require("edge-js");
-const camera = edge.func({assemblyFile:"WebCamInterface.dll", typeName:"WebCamInterface.Camera"});
+const camera = edge.func({assemblyFile:"WebCamInterface.dll", typeName:"WebCamInterface.Camera", methodName:"Capture"});
+const cameraLister = edge.func({assemblyFile:"WebCamInterface.dll", typeName:"WebCamInterface.Camera", methodName:"ListCameras"});
 module.exports = {
     grab:function(cameraMoniker, callBack){
         camera(cameraMoniker, callBack);
+    },
+    cameras: function(param,callBack){
+        cameraLister(param, callBack);
     }
 }
--- test.js
+++ test.js
@@ -1,16 +1,28 @@
 var assert = require('assert');
 var directcam;
 var fs = require("fs");
-describe("the directcam module", function(){
-    it("should exist", function(){
+describe("the directcam module", function () {
+    it("should exist", function () {
         directcam = require("./main");
         assert(directcam);
     });
-    it("should work by grabbing a picture", function(){
-        directcam.grab(null, function(err, data){
+    it("should list cameras", function (done) {
+        this.timeout(5000);
+
+        directcam.cameras(null, function (err, data) {
+            assert(data);
+            fs.writeFileSync("cameras.json", JSON.stringify(data));
+            done();
+        });
+    });
+    it("should work by grabbing a picture", function (done) {
+        this.timeout(5000);
+
+        directcam.grab(null, function (err, data) {
             assert.equal(err, undefined);
             assert(data);
             fs.writeFileSync("test.jpg", data, 'binary');
+            done();
         });
     })
 });
